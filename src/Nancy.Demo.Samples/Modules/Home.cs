13ec46c9927f60690650894a1d0823b9ad81dbf8
Andreas Hakansson <andreas@selfinflicted.org>
2013-01-12 10:52:17 +0100

    Explored reading from Mongo

--- src/Nancy.Demo.Samples/Modules/Home.cs
+++ src/Nancy.Demo.Samples/Modules/Home.cs
@@ -1,19 +1,25 @@
 ﻿namespace Nancy.Demo.Samples.Modules
 {
+    using System;
     using System.Linq;
     using Data;
     using Models;
     using MongoDB.Driver;
+    using MongoDB.Driver.Builders;
 
     public class Home : NancyModule
     {
         public Home(IDemoRepository repository, MongoDatabase database)
         {
             Get["/"] = parameters => {
-                var model = 
-                    repository.GetAll().OrderBy(x => x.Name).ThenBy(x => x.Author);
-                
-                return Negotiate.WithModel(model).WithView("index");
+                //var model = 
+                //    repository.GetAll().OrderBy(x => x.Name).ThenBy(x => x.Author);
+
+                var collection = database
+                    .GetCollection<DemoModel>("demos")
+                    .Find(Query<DemoModel>.Where(x => !string.IsNullOrEmpty(x.Id)));
+
+                return Negotiate.WithModel(collection).WithView("index");
             };
 
             Get["/about"] = parameters => {
@@ -26,8 +32,19 @@
 
             Get["/mongo/{author}"] = x =>
             {
-                var entity =
-                    new DemoModel() { Author = (string)x.author };
+                var entity = 
+                    new DemoModel
+                        {
+                            Author = (string)x.author,
+                            Description = "Created using Mongo",
+                            Gravatar = "https://secure.gravatar.com/avatar/61f3187a94eb0411ef92b123d7a13584?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
+                            HasNuget = true,
+                            Id = Guid.NewGuid().ToString(),
+                            IndexedAt = DateTime.Now,
+                            LastCommit = DateTime.Now,
+                            Name = "NoPassword",
+                            Packages = Enumerable.Empty<Tuple<string, string>>()
+                        };
 
                 var collection =
                     database.GetCollection<DemoModel>("demos");
