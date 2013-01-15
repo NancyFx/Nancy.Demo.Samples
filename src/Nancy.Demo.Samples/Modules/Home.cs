47bdcf9a4d147348445bf83a4934f79a1e3c4e2c
Andreas Hakansson <andreas@selfinflicted.org>
2013-01-15 08:12:09 +0100

    Cleaned up home module

--- src/Nancy.Demo.Samples/Modules/Home.cs
+++ src/Nancy.Demo.Samples/Modules/Home.cs
@@ -1,25 +1,18 @@
 ﻿namespace Nancy.Demo.Samples.Modules
 {
-    using System;
     using System.Linq;
     using Data;
-    using Models;
     using MongoDB.Driver;
-    using MongoDB.Driver.Builders;
 
     public class Home : NancyModule
     {
-        public Home(IDemoRepository repository, MongoDatabase database)
+        public Home(IDemoRepository repository)
         {
             Get["/"] = parameters => {
-                //var model = 
-                //    repository.GetAll().OrderBy(x => x.Name).ThenBy(x => x.Author);
+                var model = 
+                    repository.GetAll().OrderBy(x => x.Name).ThenBy(x => x.Author);
 
-                var collection = database
-                    .GetCollection<DemoModel>("demos")
-                    .Find(Query<DemoModel>.Where(x => !string.IsNullOrEmpty(x.Id)));
-
-                return Negotiate.WithModel(collection).WithView("index");
+                return Negotiate.WithModel(model).WithView("index");
             };
 
             Get["/about"] = parameters => {
@@ -28,30 +21,6 @@
 
             Get["/login"] = parameters => {
                 return View["login"];
-            };
-
-            Get["/mongo/{author}"] = x =>
-            {
-                var entity = 
-                    new DemoModel
-                        {
-                            Author = (string)x.author,
-                            Description = "Created using Mongo",
-                            Gravatar = "https://secure.gravatar.com/avatar/61f3187a94eb0411ef92b123d7a13584?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png",
-                            HasNuget = true,
-                            Id = Guid.NewGuid().ToString(),
-                            IndexedAt = DateTime.Now,
-                            LastCommit = DateTime.Now,
-                            Name = "NoPassword",
-                            Packages = Enumerable.Empty<Tuple<string, string>>()
-                        };
-
-                var collection =
-                    database.GetCollection<DemoModel>("demos");
-
-                collection.Insert(entity);
-
-                return 200;
             };
         }
     }
