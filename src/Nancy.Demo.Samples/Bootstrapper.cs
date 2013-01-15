5c2c06555e77dd923d3fac5529d37fa8280ae5ba
Andreas Hakansson <andreas@selfinflicted.org>
2013-01-15 08:06:40 +0100

    Temporarily changed the Mongo registrations

--- src/Nancy.Demo.Samples/Bootstrapper.cs
+++ src/Nancy.Demo.Samples/Bootstrapper.cs
@@ -8,24 +8,26 @@
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        private const string ConnectionString = @"mongodb://test:test@linus.mongohq.com:10000";
+
         protected override void ConfigureApplicationContainer(TinyIoCContainer container)
         {
             base.ConfigureApplicationContainer(container);
-
-            var connectionString = @"mongodb://test:test@linus.mongohq.com:10000";
-            var client = new MongoClient(connectionString);
+            
+            var client = new MongoClient(ConnectionString);
             var server = client.GetServer();
 
             container.Register((c, p) => client);
             container.Register((c, p) => server);
+
+            container.Register((c, p) => server.GetDatabase("Demos"));
         }
 
         protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
         {
             base.ConfigureRequestContainer(container, context);
 
-            var server = container.Resolve<MongoServer>();
-            container.Register((c, p) => server.GetDatabase("Demos"));
+           
         }
 
         protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
