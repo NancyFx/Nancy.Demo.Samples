4694dc1b0bdd42055fc50579fb1038a576d00958
Andreas Hakansson <andreas@selfinflicted.org>
2013-01-20 00:45:02 +0100

    Updated registrations

--- src/Nancy.Demo.Samples/Bootstrapper.cs
+++ src/Nancy.Demo.Samples/Bootstrapper.cs
@@ -2,12 +2,14 @@
 {
     using Authentication.Forms;
     using Cryptography;
+    using Data;
     using MongoDB.Driver;
     using Nancy.Bootstrapper;
     using Nancy.TinyIoc;
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        //private const string ConnectionString = @"mongodb://localhost:27017";
         private const string ConnectionString = @"mongodb://test:test@linus.mongohq.com:10000";
 
         protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
@@ -34,12 +36,17 @@
             base.ConfigureApplicationContainer(container);
             
             var client = new MongoClient(ConnectionString);
-            var server = client.GetServer();
+            container.Register((c, p) => client.GetServer());
+        }
 
-            container.Register((c, p) => client);
-            container.Register((c, p) => server);
+        protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
+        {
+            base.ConfigureRequestContainer(container, context);
 
+            var server = container.Resolve<MongoServer>();
             container.Register((c, p) => server.GetDatabase("Demos"));
+            container.Register<IDemoRepository, DefaultDemoRepository>();
+            container.Register<IContributorRepository, DefaultContributorRepository>();
         }
     }
 }
