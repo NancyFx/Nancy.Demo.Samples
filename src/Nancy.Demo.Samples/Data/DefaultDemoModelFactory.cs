5a180456409d67c1e624c2a6472dae7be3cee027
Andreas Hakansson <andreas@selfinflicted.org>
2013-02-15 13:10:29 +0100

    Moved type to own file

--- src/Nancy.Demo.Samples/Data/DefaultDemoModelFactory.cs
+++ src/Nancy.Demo.Samples/Data/DefaultDemoModelFactory.cs
@@ -151,20 +151,4 @@
             });
         }
     }
-
-    /// <summary>
-    /// Stores information about a Nuget package.
-    /// </summary>
-    public class NugetPackageInformation
-    {
-        /// <summary>
-        /// The name of the package.
-        /// </summary>
-        public string Name { get; set; }
-
-        /// <summary>
-        /// The version of the package.
-        /// </summary>
-        public string Version { get; set; }
-    }
 }
