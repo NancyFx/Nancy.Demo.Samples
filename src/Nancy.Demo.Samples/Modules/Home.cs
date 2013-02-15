3433a94e2559ba01f27f835aedb2d0d66b433d81
Andreas Hakansson <andreas@selfinflicted.org>
2013-02-15 13:24:10 +0100

    Cleaned up the modules a bit

--- src/Nancy.Demo.Samples/Modules/Home.cs
+++ src/Nancy.Demo.Samples/Modules/Home.cs
@@ -1,48 +1,16 @@
 ﻿namespace Nancy.Demo.Samples.Modules
 {
-    using System;
-    using Authentication.Forms;
-
     public class Home : NancyModule
     {
-        public Home(IUserDatabase userDatabase)
+        public Home()
         {
             Get["/"] = parameters =>
-                           {
-
-                               var user = Context.CurrentUser;
+            {
                 return View["index"];
             };
 
             Get["/about"] = parameters => {
                 return View["about"];
-            };
-
-            Get["/login"] = x =>
-            {
-                return View["login"];
-            };
-
-            Post["/login"] = parameters => {
-                var userGuid = userDatabase.ValidateUser((string)this.Request.Form.Password);
-
-                if (userGuid == null)
-                {
-                    //return this.Context.GetRedirect("~/login?error=true&username=" + (string)this.Request.Form.Username);
-                    return Response.AsRedirect("/");
-                }
-
-                DateTime? expiry = null;
-                //if (this.Request.Form.RememberMe.HasValue)
-                //{
-                //    expiry = DateTime.Now.AddDays(7);
-                //}
-
-                return this.LoginAndRedirect(userGuid.Value, expiry);
-            };
-
-            Get["/logout"] = x => {
-                return this.LogoutAndRedirect("~/");
             };
         }
     }
--- src/Nancy.Demo.Samples/Modules/Login.cs
+++ src/Nancy.Demo.Samples/Modules/Login.cs
@@ -1,9 +1,36 @@
 namespace Nancy.Demo.Samples.Modules
 {
+    using System;
+    using Authentication.Forms;
+
     public class Login : NancyModule
     {
-        public Login()
+        public Login(IUserDatabase userDatabase)
         {
+
+            Get["/login"] = x =>
+            {
+                return View["login"];
+            };
+
+            Post["/login"] = parameters =>
+            {
+                var userGuid = userDatabase.ValidateUser((string)this.Request.Form.Password);
+
+                if (userGuid == null)
+                {
+                    return Response.AsRedirect("/");
+                }
+
+                DateTime? expiry = null;
+
+                return this.LoginAndRedirect(userGuid.Value, expiry);
+            };
+
+            Get["/logout"] = x =>
+            {
+                return this.LogoutAndRedirect("~/");
+            };
         }
     }
 }
